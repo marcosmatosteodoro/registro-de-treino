808fa88d29659774f3651a68588e92a5773073a6
Marcos Paulo Teodoro <mteodoro-ext@techforti.com.br>
2025-10-28 08:06:08 -0300

    Refactor user selection to use buttons instead of links, integrating user data retrieval and navigation to the Treinos page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
-import Link from "next/link";
+"use client";
+
+import { GetUsers } from "@/services/getUsers";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const users = GetUsers.getAll();
+  const router = useRouter();
+
+  const handleUserSelection = (userId: number) => {
+    // Aqui você pode adicionar outras lógicas depois
+    console.log(`Usuário selecionado: ${userId}`);
+    
+    // Redireciona para a página de treinos
+    router.push("/treinos");
+  };
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-linear-to-br from-gray-900 to-black">
       <main className="flex flex-col items-center justify-center px-8 py-16 text-center">
@@ -9,19 +23,19 @@ export default function Home() {
         </h1>
         
         <div className="flex flex-col gap-6 sm:flex-row sm:gap-8">
-          <Link 
-            href="/treinos"
-            className="flex items-center justify-center w-48 h-16 bg-linear-to-r from-orange-500 to-red-600 hover:from-orange-600 hover:to-red-700 text-white font-bold text-xl rounded-lg shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-xl"
+          <button 
+            onClick={() => handleUserSelection(users[0].id)}
+            className="w-48 h-16 bg-linear-to-r from-orange-500 to-red-600 hover:from-orange-600 hover:to-red-700 text-white font-bold text-xl rounded-lg shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-xl"
           >
-            Marcos
-          </Link>
+            {users[0].nome}
+          </button>
           
-          <Link 
-            href="/treinos"
-            className="flex items-center justify-center w-48 h-16 bg-linear-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700 text-white font-bold text-xl rounded-lg shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-xl"
+          <button 
+            onClick={() => handleUserSelection(users[1].id)}
+            className="w-48 h-16 bg-linear-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700 text-white font-bold text-xl rounded-lg shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-xl"
           >
-            Evelin
-          </Link>
+            {users[1].nome}
+          </button>
         </div>
       </main>
     </div>
